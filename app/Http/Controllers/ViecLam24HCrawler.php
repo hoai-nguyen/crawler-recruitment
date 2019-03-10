af439f9e0db98a7f3e5dfbb2d0d0aa21f7a50b51
Hoai Nguyen <nguyenvanhoai.cs@gmail.com>
2019-03-10 19:42:27 +0900

    Update ViecLam24HCrawler.php

--- app/Http/Controllers/ViecLam24HCrawler.php
+++ app/Http/Controllers/ViecLam24HCrawler.php
@@ -22,7 +22,7 @@ class ViecLam24HCrawler extends Controller{
 	const DATE_FORMAT = "Ymd";
 	const SLASH = DIRECTORY_SEPARATOR;
 	const BATCH_SIZE = 3;
-	const MAX_PAGE = 1000;
+	const MAX_PAGE = 150;
 	const EMAIL_PATTERN = "/[a-z0-9_\-\+\.]+@[a-z0-9\-]+\.([a-z]{2,4})(?:\.[a-z]{2})?/i";
 	const PHONE_PATTERN = "!\d+!";
 
