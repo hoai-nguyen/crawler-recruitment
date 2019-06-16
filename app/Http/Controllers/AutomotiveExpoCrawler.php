9b0c3882f39fc8893e78f378b5ef0e9c561f09b4
HoaiNV <hoainv@fsoft.com.vn>
2019-06-17 02:51:16 +0900

    add all expo

--- app/Http/Controllers/AutomotiveExpoCrawler.php
+++ app/Http/Controllers/AutomotiveExpoCrawler.php
@@ -24,78 +24,89 @@ class AutomotiveExpoCrawler extends Controller{
 		$start = microtime(true);
 		error_log("Start crawling AutomotiveExpo ...");
 
-		$file_path = public_path('data').self::SLASH.self::DATA_PATH.self::SLASH;
+		
 		try {
 			$database = env("DB_DATABASE");
 			if ($database == null)  $database = Common::DB_DEFAULT;
 
-			$pageUrl = "https://jan2019.tems-system.com/exhiSearch/AUTO/eng/ExhiList";
-			$client = new Client(); 
-			$crawler = $client -> request('GET', $pageUrl);
+			$exhibitions = array(
+				"https://jan2019.tems-system.com/exhiSearch/AUTO/eng/"
+				, "http://jan2019.tems-system.com/exhiSearch/ROBO/eng/"
+				, "http://jan2019.tems-system.com/exhiSearch/SFE/eng/"
+				, "http://jan2019.tems-system.com/exhiSearch/INW/eng/"
+				, "http://jan2019.tems-system.com/exhiSearch/WEA/eng/"
+			);
+			foreach( $exhibitions as $exhibition){
+				$exhibitionUrl = $exhibition."ExhiList";
+				error_log($exhibitionUrl);
+				$client = new Client(); 
+				$crawler = $client -> request('GET', $exhibitionUrl);
+				$exhibitors = $crawler -> filter("#01") -> filter("li");
+				$idx = 0;
+				foreach( $exhibitors as $el){
+					error_log($idx);
+					$idx = $idx + 1;
+					if ($idx > 5){
+						break;
+					}
+					$start = microtime(true);
 
-			$exhibitors = $crawler -> filter("#01") -> filter("li");
-			$infos = array();
-			
-			$idx = 0;
-			foreach( $exhibitors as $el){
-				$idx = $idx + 1;
-				if ($idx > 30){
-					break;
-				}
-				$start = microtime(true);
-
-				$company_crl = new Crawler($el);
-				$company = $company_crl -> text();
-				$atag = $company_crl -> filter("a");
-				$tel = "";
-				$website = "";
-				$email = "";
-				
-				if ($atag -> count() > 0){
-					$id = $atag -> attr("val-id");
-					$type = $atag -> attr("val-type");
-					$link = "https://jan2019.tems-system.com/exhiSearch/AUTO/eng/Details?id=$id&type=$type";
-					$client = new Client(); 
-					$page_crawler = $client -> request('GET', $link);
-					$details = $page_crawler -> filter("div.search_detail_info") -> filter("td");
-					foreach( $details as $node){
-						$crl = new Crawler($node);
-						$text = $crl -> text();
-						if (strpos($text, 'TEL') !== false){
-							$tel_arr = preg_split("/：/", $text);
-							$tel = "";
-							if (sizeof($tel_arr) > 1){
-								$tel = Common::RemoveTrailingChars($tel_arr[1]);
-							}
-						} else if (strpos($text, 'URL') !== false){
-							$website_crl = $crl -> filter("a");
-							if ($website_crl -> count() > 0){
-								$website = $website_crl -> attr("href");
-								if (strpos($website, 'http') !== false){
-									$page_crl = $client -> request('GET', $website);
-									$email = Common::ExtractEmailFromText($page_crl -> text());
+					$company_crl = new Crawler($el);
+					$company = $company_crl -> text();
+					$atag = $company_crl -> filter("a");
+					$tel = "";
+					$website = "";
+					$email = "";
+					
+					if ($atag -> count() > 0){
+						$id = $atag -> attr("val-id");
+						$type = $atag -> attr("val-type");
+						$link =  $exhibition."Details?id=$id&type=$type";
+						error_log("Link = ".$link);
+						$client = new Client(); 
+						$page_crawler = $client -> request('GET', $link);
+						$details = $page_crawler -> filter("div.search_detail_info") -> filter("td");
+						foreach( $details as $node){
+							$crl = new Crawler($node);
+							$text = $crl -> text();
+							if (strpos($text, 'TEL') !== false){
+								$tel_arr = preg_split("/：/", $text);
+								$tel = "";
+								if (sizeof($tel_arr) > 1){
+									$tel = Common::RemoveTrailingChars($tel_arr[1]);
+								}
+							} else if (strpos($text, 'URL') !== false){
+								$website_crl = $crl -> filter("a");
+								if ($website_crl -> count() > 0){
+									$website = $website_crl -> attr("href");
+									if (strpos($website, 'http') !== false){
+										$page_crl = $client -> request('GET', $website);
+										if ($page_crl -> count() > 0){
+											$email = Common::ExtractEmailFromText($page_crl -> text());
+										}
+									}
 								}
 							}
 						}
 					}
-				}
-				// $data = "company: ".$company.", website: ".$website.", tel: ".$tel.", email: ".$email;
-				// echo $data."<br>";
-				error_log("company: ".$company.", website: ".$website.", tel: ".$tel.", email: ".$email);
+					error_log("company: ".$company.", website: ".$website.", tel: ".$tel.", email: ".$email);
 
-				$exhibitor_data = array(
-					$company
-					, $tel
-					, $email
-					, $website
-				);
-				if (!Common::IsNullOrEmpty($email) or !Common::IsNullOrEmpty($tel) or !Common::IsNullOrEmpty($email)){
-					Common::AppendArrayToFile($exhibitor_data, $file_path.self::EXHIBITOR_DATA.'.csv', "|");
-				} else {
-					Common::AppendArrayToFile($exhibitor_data, $file_path.self::EXHIBITOR_DATA_NO_CONTACT.'.csv', "|");
+					$exhibitor_data = array(
+						$company
+						, $tel
+						, $email
+						, $website
+					);
+
+					$file_path = public_path('data').self::SLASH.self::DATA_PATH.self::SLASH;
+
+					if (!Common::IsNullOrEmpty($website) or !Common::IsNullOrEmpty($tel)){
+						Common::AppendArrayToFile($exhibitor_data, $file_path.self::EXHIBITOR_DATA.'.csv', "|");
+					} else {
+						Common::AppendArrayToFile($exhibitor_data, $file_path.self::EXHIBITOR_DATA_NO_CONTACT.'.csv', "|");
+					}
 				}
 			}
-
 		 } catch (\Exception $e) {
 		 	error_log($e -> getMessage());
 		 }
